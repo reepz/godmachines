04e7857f40d2f4f4f54e7a2c7b1eb9be499b004e
Reepz <a.wojtowicz@outlook.com>
2022-03-04 00:41:57 +0100

    disable html elements selection and dragging (more native feel)

--- main.js
+++ main.js
@@ -73,6 +73,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
